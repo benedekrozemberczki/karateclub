0cb63b0355991bdcd6c724f7e5c5d8914dc648f1
Benedek Rozemberczki <benedek.rozemberczki@gmail.com>
2019-12-05 22:36:42 +0000

    Add files via upload

---
+++ karateclub/Overlapping.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class ClassD(object):
+    def __init__(self):
+        self.x = "A"
+        self.y = np.ones(5)
+
+class ClassE(object):
+    def __init__(self):
+        self.x = "B"
+
+class ClassF(object):
+    def __init__(self):
+        self.x = "C"
+
+
