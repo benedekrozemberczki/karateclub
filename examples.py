edd053f63d2aa6dd036085e6e1fff372edf2f205
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2019-12-06 18:19:58 +0000

    updates

--- examples.py
+++ examples.py
@@ -1,12 +1,26 @@
 """Example runs with Karate Club."""
 
 import networkx as nx
-from karateclub import EgoNetSplitter
+from karateclub import EgoNetSplitter, EdMot
 
 g = nx.newman_watts_strogatz_graph(1000, 20, 0.05)
+
+#------------------------------------
+# Splitter example
+#------------------------------------
 
 splitter = EgoNetSplitter(1.0)
 
 splitter.fit(g)
 
 print(splitter.get_memberships())
+
+#------------------------------------
+# Edmot example
+#------------------------------------
+
+edmot = EdMot(2, 0.5)
+
+edmot.fit(g)
+
+print(edmot.get_memberships())
