3b0acd5e4e6e9afc135c9f4df49e53920bbd233d
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2019-12-06 18:28:41 +0000

    EdMot

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = "karateclub",
   packages = ["karateclub"],
-  version = "0.7",
+  version = "0.8",
   license = "MIT",
   description = "A general purpose library for community detection and graph clustering research.",
   author = "Benedek Rozemberczki",
   author_email = "benedek.rozemberczki@gmail.com",
   url = "https://github.com/benedekrozemberczki/karateclub",
-  download_url = "https://github.com/benedekrozemberczki/karateclub/archive/v_07.tar.gz",
+  download_url = "https://github.com/benedekrozemberczki/karateclub/archive/v_08.tar.gz",
   keywords = ["community", "detection", "networkx", "graph", "clustering"],
   install_requires=[
           "numpy",
