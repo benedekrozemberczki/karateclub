fe1e586fe902653c3a4208f376cff17f50bccf1a
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2019-12-28 21:21:04 +0000

    BigClam

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import find_packages
 setup(
   name = "karateclub",
   packages = find_packages(),
-  version = "0.25",
+  version = "0.26",
   license = "MIT",
   description = "A general purpose library for community detection and graph embedding research.",
   author = "Benedek Rozemberczki",
   author_email = "benedek.rozemberczki@gmail.com",
   url = "https://github.com/benedekrozemberczki/karateclub",
-  download_url = "https://github.com/benedekrozemberczki/karateclub/archive/v_025.tar.gz",
+  download_url = "https://github.com/benedekrozemberczki/karateclub/archive/v_026.tar.gz",
   keywords = ["community", "detection", "networkx", "graph", "clustering"],
   install_requires=[
           "numpy",
