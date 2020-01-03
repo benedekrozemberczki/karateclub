3c5908b0ba796a52b93eb149cf17c82afe8e4a5a
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-01-03 14:46:54 +0000

    Meta Class.

--- karateclub/karateclubestimator.py
+++ karateclub/karateclubestimator.py
@@ -2,18 +2,26 @@
 
 class Estimator(object):
 
+    """Estimator meta class with constructor and public methods."""
+
     def __init__(self):
-    
+        """Creatinng an estimator."""
+        pass
+
     def fit(self):
+        """Fitting a model."""
         pass
 
     def get_embedding(self):
+        """Getting the embeddings."""
         return None
 
     def get_memberships(self):
+        """Getting the membership dictionary."""
         return None
 
     def get_cluster_centers(self):
+        """Getting the cluster centers."""
         return None
 
 
