bf56c2cc8170d8071363e2313794373663000f54
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-01-04 17:38:48 +0000

    Adding BoostNE

--- karateclub/node_embedding/neighbourhood/boostne.py
+++ karateclub/node_embedding/neighbourhood/boostne.py
@@ -16,13 +16,12 @@ class BoostNE(Estimator):
         dimensions (int): Number of individual embedding dimensions. Default is 32.
         iteration (int): Number of SVD iterations. Default is 10.
         order (int): Number of PMI matrix powers. Default is 5
-        seed (int): SVD random seed. Default is 42.
+
     """
     def __init__(self, dimensions=32, iteration=10, order=5, seed=42):
         self.dimensions = dimensions
         self.iterations = iteration
         self.order = order
-        self.seed = seed
 
     def _create_D_inverse(self, graph):
         """
@@ -45,41 +44,27 @@ class BoostNE(Estimator):
         Creating a tuple with the normalized adjacency matrix.
 
         Return types:
-            * **(A_hat, A_hat)** *(Tuple of SciPy arrays)* - Normalized adjacencies.
+            * **(A_hat, A_hat, A_hat)** *(Tuple of SciPy arrays)* - Normalized adjacencies.
         """
         A = nx.adjacency_matrix(graph, nodelist=range(graph.number_of_nodes()))
         D_inverse = self._create_D_inverse(graph)
         A_hat = D_inverse.dot(A)
-        return (A_hat, A_hat)
+        return (A_hat, A_hat, A_hat)
 
-    def _create_target_matrix(self):
+    def _create_target_matrix(self, graph):
         """
         Creating a log transformed target matrix.
 
         Return types:
             * **target_matrix** *(SciPy array)* - The PMI matrix.
         """
-        self.A_tilde = sparse.coo_matrix(self.A_tilde.dot(self.A_hat))
-        scores = np.log(self.A_tilde.data)-math.log(self.A_tilde.shape[0])
-        rows = self.A_tilde.row[scores < 0]
-        cols = self.A_tilde.col[scores < 0]
-        scores = scores[scores < 0]
-        target_matrix = sparse.coo_matrix((scores, (rows, cols)),
-                                          shape=self.A_tilde.shape,
-                                          dtype=np.float32)
+        A_tilde, A_hat, A_accum = self._create_base_matrix(graph)
+        for _ in range(self.order-1):
+            A_tilde = sparse.coo_matrix(A_tilde.dot(A_hat))
+            A_accum = A_accum + A_tilde
+        A_accum = A_accum / self.order
+        return A_accum
 
-        return target_matrix
-
-    def _create_single_embedding(self, target_matrix):
-        """
-        Fitting a single SVD embedding of a PMI matrix.
-        """
-        svd = TruncatedSVD(n_components=self.dimensions,
-                           n_iter=self.iterations,
-                           random_state=self.seed)
-        svd.fit(target_matrix)
-        embedding = svd.transform(target_matrix)
-        self.embeddings.append(embedding)
 
     def fit(self, graph):
         """
@@ -88,13 +73,12 @@ class BoostNE(Estimator):
         Arg types:
             * **graph** *(NetworkX graph)* - The graph to be embedded.
         """
-        self.A_tilde, self.A_hat = self._create_base_matrix(graph)
-        self.embeddings = []
-        target_matrix = self._create_target_matrix()
+        target_matrix = self._create_target_matrix(graph)
         self._create_single_embedding(target_matrix)
-        for step in range(self.order-1):
-            target_matrix = self._create_target_matrix()
-            self._create_single_embedding(target_matrix)
+
+
+
+
 
     def get_embedding(self):
         r"""Getting the node embedding.
