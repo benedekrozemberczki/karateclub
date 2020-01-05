457a174a66f71bb0efe97d6c5315f7c30d612d8b
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-01-05 14:54:33 +0000

    Graph2Vec.

--- karateclub/__init__.py
+++ karateclub/__init__.py
@@ -1,5 +1,6 @@
 from karateclub.community_detection import *
 from karateclub.node_embedding import *
+from karateclub.graph_embedding import *
 from karateclub.utils import *
 from karateclub.estimator import Estimator
 
