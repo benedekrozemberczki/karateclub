e9d8cd459a0c20bcc3e4956673865ab401f62114
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-01-05 16:30:06 +0000

    hashlib fix

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import find_packages
 setup(
   name = "karateclub",
   packages = find_packages(),
-  version = "0.30",
+  version = "0.31",
   license = "MIT",
   description = "A general purpose library for community detection and graph embedding research.",
   author = "Benedek Rozemberczki",
   author_email = "benedek.rozemberczki@gmail.com",
   url = "https://github.com/benedekrozemberczki/karateclub",
-  download_url = "https://github.com/benedekrozemberczki/karateclub/archive/v_030.tar.gz",
+  download_url = "https://github.com/benedekrozemberczki/karateclub/archive/v_031.tar.gz",
   keywords = ["community", "detection", "networkx", "graph", "clustering"],
   install_requires=[
           "numpy",
@@ -20,8 +20,7 @@ setup(
           "sklearn",
           "scipy",
           "pygsp",
-          "gensim",
-          "hashlib",
+          "gensim"
       ],
   classifiers=[
     "Development Status :: 3 - Alpha",
