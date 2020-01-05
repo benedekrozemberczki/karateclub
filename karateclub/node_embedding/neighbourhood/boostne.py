c519ef7b172b55a5f6045f660c6b241549764b90
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-01-05 14:01:22 +0000

    BoostNE

--- karateclub/node_embedding/neighbourhood/boostne.py
+++ karateclub/node_embedding/neighbourhood/boostne.py
@@ -16,7 +16,7 @@ class BoostNE(Estimator):
     Args:
         dimensions (int): Number of individual embedding dimensions. Default is 4.
         iteration (int): Number of boosting iterations. Default is 10.
-        order (int): Number of PMI matrix powers. Default is 5.
+        order (int): Number of adjacency matrix powers. Default is 5.
         alpha (float): NMF regularization parameter. 0.01.
     """
     def __init__(self, dimensions=4, iteration=10, order=5, alpha=0.01):
