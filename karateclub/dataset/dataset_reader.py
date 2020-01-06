1e994ecf580712c74e406a58b3aa957094012783
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-01-06 23:34:17 +0000

    Dataset.

--- karateclub/dataset/dataset_reader.py
+++ karateclub/dataset/dataset_reader.py
@@ -1,10 +1,10 @@
+import io
 import os
+import json
 import numpy as np
 import pandas as pd
 import networkx as nx
 from six.moves import urllib
-import io
-import json
 from scipy.sparse import coo_matrix
 
 def pandas_reader(bytes):
@@ -17,24 +17,40 @@ def pandas_reader(bytes):
     return tab
 
 class GraphReader(object):
+    r"""Class to read benchmark datasets for the community detection or node embedding task.
 
-    def __init__(self, dataset):
+    Args:
+        dataset (str): Dataset of interest  on of facebook/wikipedia/github/twitch. Default is 'wikipedia'.
+    """
+    def __init__(self, dataset="wikipedia"):
         self.dataset = dataset
         self.base_url = "https://github.com/benedekrozemberczki/karateclub/raw/master/dataset/node_level/"
 
     def _dataset_reader(self, end):
+        """
+        Reading the dataset from the web.
+        """
         path = os.path.join(self.base_url, self.dataset, end)
         data = urllib.request.urlopen(path)
         return data
    
     def get_graph(self):
+        r"""Getting the graph.
+
+        Return types:
+            * **graph** *(NetworkX graph)* - Graph of interest.
+        """
         data = self._dataset_reader("edges.csv")
         data = pandas_reader(data.read())
         graph = nx.convert_matrix.from_pandas_edgelist(data, "id_1", "id_2")
-
         return graph
 
     def get_features(self):
+        r"""Getting the node features Scipy matrix.
+
+        Return types:
+            * **features** *(COO Scipy array)* - Node feature matrix.
+        """
         data = self._dataset_reader("features.json")
         data = json.loads(data.read().decode())
         row = [int(k) for k, v in data.items() for val in v]
@@ -47,6 +63,11 @@ class GraphReader(object):
         return features
 
     def get_target(self):
+        r"""Getting the class membership of nodes.
+
+        Return types:
+            * **target** *(Numpy array)* - Class membership vector.
+        """
         data = self._dataset_reader("target.csv")
         data = pandas_reader(data.read())
         target = np.array(data["target"])
