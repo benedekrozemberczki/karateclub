4ee19f4f69e5a02fae6d32cf0dce874f90813ea7
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-01-09 14:21:48 +0000

    Reddit 10K dataset

--- karateclub/dataset/dataset_reader.py
+++ karateclub/dataset/dataset_reader.py
@@ -79,7 +79,7 @@ class GraphSetReader(object):
     Args:
         dataset (str): Dataset of interest  on of facebook/wikipedia/github/twitch. Default is 'wikipedia'.
     """
-    def __init__(self, dataset="wikipedia"):
+    def __init__(self, dataset="reddit10k"):
         self.dataset = dataset
         self.base_url = "https://github.com/benedekrozemberczki/karateclub/raw/master/dataset/graph_level/"
 
@@ -99,34 +99,7 @@ class GraphSetReader(object):
         """
         path = os.path.join(self.base_url, self.dataset, end)
         data = urllib.request.urlopen(path).read()
-        data = self._pandas_reader(data)
         return data
-   
-    def get_graphs(self):
-        r"""Getting the graph.
-
-        Return types:
-            * **graph** *(NetworkX graph)* - Graph of interest.
-        """
-        data = self._dataset_reader("edges.csv")
-        graph = nx.convert_matrix.from_pandas_edgelist(data, "id_1", "id_2")
-        return graph
-
-    def get_features(self):
-        r"""Getting the node features Scipy matrix.
-
-        Return types:
-            * **features** *(COO Scipy array)* - Node feature matrix.
-        """
-        data = self._dataset_reader("features.csv")
-        row = np.array(data["node_id"])
-        col = np.array(data["feature_id"])
-        values = np.array(data["value"])
-        node_count = max(row) + 1
-        feature_count = max(col) + 1
-        shape = (node_count, feature_count)
-        features = coo_matrix((values, (row, col)), shape=shape)
-        return features
 
     def get_target(self):
         r"""Getting the class membership of nodes.
@@ -135,5 +108,6 @@ class GraphSetReader(object):
             * **target** *(Numpy array)* - Class membership vector.
         """
         data = self._dataset_reader("target.csv")
-        target = np.array(data["target"])
+        data_tab = self._pandas_reader(data)
+        target = np.array(data_tab["target"])
         return target
