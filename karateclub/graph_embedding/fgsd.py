8327f2601bf872463815c6e454dc805b4839544c
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-01-13 11:04:58 +0000

    files

--- karateclub/graph_embedding/fgsd.py
+++ karateclub/graph_embedding/fgsd.py
@@ -33,7 +33,9 @@ class FGSD(Estimator):
         self.min_count = min_count
 
     def _calculate_fgsd(self, graph):
-        pass
+        a = nx.laplacian_matrix(graph)
+        u, s, vt = svds(a,k=min(a.shape)-1)
+        fL = vt.T @ np.diag(1/s) @ u.T
 
     def fit(self, graphs):
         """
