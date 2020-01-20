408e0939aedc14382db67900349acbc4ed7fba3b
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-01-20 22:49:44 +0000

    FSCNMF

--- karateclub/node_embedding/attributed/fscnmf.py
+++ karateclub/node_embedding/attributed/fscnmf.py
@@ -1,11 +1,13 @@
 import numpy as np
 import networkx as nx
 from scipy import sparse
+from numpy.linalg import inv
 from karateclub.estimator import Estimator
 
 class FSCNMF(Estimator):
-    r"""An implementation of `"TENE" <https://ieeexplore.ieee.org/document/8545577>`_
-    from the ICPR '18 paper "Enhanced Network Embedding with Text Information". The 
+    r"""An implementation of `"TENE" <https://arxiv.org/pdf/1804.05313.pdf.>`_
+    from the Arxiv '18 paper "Fusing Structure and Content via Non-negative Matrix
+    Factorization for Embedding Information Networks". The 
     procedure jointly factorizes the adjacency and node feature matrices using alternating
     least squares.
        
@@ -16,15 +18,19 @@ class FSCNMF(Estimator):
         beta (float): Feature matrix regularizer coefficient. Default is 0.1.
         iterations (int): ALS iterations. Default is 200.
     """
-    def __init__(self, dimensions=32, lower_control=10**-15, iterations=100,
-                 alpha_1=1.0, alpha_2=1.0, beta_1=1.0, beta_2=1.0, gamma=1.0):
+    def __init__(self, dimensions=32, lower_control=10**-15, iterations=500,
+                 alpha_1=1000.0, alpha_2=1.0, alpha_3=1.0, beta_1=1000.0,
+                 beta_2=1.0, beta_3=1.0, gamma=0.5):
+
         self.dimensions = dimensions
         self.lower_control = lower_control
         self.iterations = iterations
         self.alpha_1 = alpha_1
         self.alpha_2 = alpha_2
+        self.alpha_3 = alpha_3
         self.beta_1 = beta_1
         self.beta_2 = beta_2
+        self.beta_3 = beta_3
         self.gamma = gamma
 
     def _init_weights(self):
@@ -36,7 +42,7 @@ class FSCNMF(Estimator):
         self.B_1 = np.random.uniform(0, 1, (self.A.shape[0], self.dimensions))
         self.B_2 = np.random.uniform(0, 1, (self.dimensions, self.A.shape[0]))
 
-    def update_B1(self):
+    def _update_B1(self):
         """
         Update node bases.
         """
@@ -47,7 +53,7 @@ class FSCNMF(Estimator):
         self.B_1 = np.dot(simi_term, covar_term)
         self.B_1[self.B_1 < self.lower_control] = self.lower_control
 
-    def update_B2(self):
+    def _update_B2(self):
         """
         Update node features.
         """
@@ -58,7 +64,7 @@ class FSCNMF(Estimator):
         self.B_2 = covar_term.dot(simi_term)
         self.B_2[self.B_2 < self.lower_control] = self.lower_control
 
-    def update_U(self):
+    def _update_U(self):
         """
         Update feature basis.
         """
@@ -69,7 +75,7 @@ class FSCNMF(Estimator):
         self.U = np.dot(simi_term, covar_term)
         self.U[self.U < self.lower_control] = self.lower_control
 
-    def update_V(self):
+    def _update_V(self):
         """
         Update features.
         """
