67c3d2cac64029e891e696f8ab0faa2ed46ddb89
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-01-27 17:09:52 +0000

    Graph2Vec Seed fix.

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import find_packages
 setup(
   name = "karateclub",
   packages = find_packages(),
-  version = "0.44",
+  version = "0.45",
   license = "MIT",
   description = "A general purpose library for community detection, network embedding, and graph mining research.",
   author = "Benedek Rozemberczki",
   author_email = "benedek.rozemberczki@gmail.com",
   url = "https://github.com/benedekrozemberczki/karateclub",
-  download_url = "https://github.com/benedekrozemberczki/karateclub/archive/v_044.tar.gz",
+  download_url = "https://github.com/benedekrozemberczki/karateclub/archive/v_045.tar.gz",
   keywords = ["community", "detection", "networkx", "graph", "clustering", "embedding","network","deepwalk","graph2vec","node2vec","deep","learning"],
   install_requires=[
           "numpy",
