bc8cc85b18a652f47a907461fce59d10d66f7737
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-02-11 19:40:23 +0000

    Walker fix

--- karateclub/utils/walker.py
+++ karateclub/utils/walker.py
@@ -6,7 +6,7 @@ class RandomWalker:
     Class to do fast first-order random walks.
 
     Args:
-        walk_length (int): Number of diffusions. Default is 10.
+        walk_length (int): Number of random walks. Default is 10.
         walk_number (int): Number of nodes in diffusion. Default is 80.
     """
     def __init__(self, walk_length, walk_number):
