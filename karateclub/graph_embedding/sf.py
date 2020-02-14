96644b1d41c30a3edcc61d5663b3cd700af276e3
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-02-14 12:18:20 +0000

    Typo fixes.

--- karateclub/graph_embedding/sf.py
+++ karateclub/graph_embedding/sf.py
@@ -7,7 +7,7 @@ class SF(Estimator):
     r"""An implementation of `"SF" <A Simple Baseline Algorithm for Graph Classification>`_
     from the NeurIPS Relational Representation Learning Workshop '18 paper "A Simple Baseline Algorithm for Graph Classification".
     The procedure calculates the k lowest egeinvalues of the normalized Laplacian.
-    If the graph has a lowe umber of eigenvalues than k the representation is padded with zeros.
+    If the graph has a lower number of eigenvalues than k the representation is padded with zeros.
 
     Args:
         dimensions (int): Number of lowest eigenvalues. Default is 128.
