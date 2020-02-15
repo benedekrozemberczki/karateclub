c2c667f2eac051251c36f4510b8b9132c899728a
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-02-15 21:56:15 +0000

    SCD added

--- karateclub/community_detection/non_overlapping/scd.py
+++ karateclub/community_detection/non_overlapping/scd.py
@@ -3,10 +3,10 @@ import networkx as nx
 from karateclub.estimator import Estimator
 
 class SCD(Estimator):
-    r"""An implementation of `"Label Propagation Clustering" <https://arxiv.org/abs/0709.2938>`_
-    from the Physical Review '07 paper "Near Linear Time Algorithm to Detect Community Structures
-    in Large-Scale Networks". The tool executes a series of label propagations with unique labels.
-    The final labels are used as cluster memberships.
+    r"""An implementation of `"SCD" <https://arxiv.org/abs/0709.2938>`_ from the
+    WWW '14 paper "High Quality, Scalable and Parallel Community Detection for 
+    Large Real Graphs". The procedure optimizes greedily the weighted community
+    clustering indirectly.
 
     Args:
         seed (int): Random seed. Default is 42.
@@ -16,35 +16,6 @@ class SCD(Estimator):
         self.seed = seed
         self.iterations = iterations
 
-    def _make_a_pick(self, neighbors):
-        """
-        Choosing a neighbor from a propagation source node.
-
-        Arg types:
-            * **neigbours** *(list)* - Neighbouring nodes.
-        """
-        scores = {}
-        for neighbor in neighbors:
-            neighbor_label = self.labels[neighbor]
-            if neighbor_label in scores.keys():
-                scores[neighbor_label] = scores[neighbor_label] + 1
-            else:
-                scores[neighbor_label] = 1
-        top = [key for key, val in scores.items() if val == max(scores.values())]
-        return random.sample(top, 1)[0]
-
-    def _do_a_propagation(self):
-        """
-        Doing a propagation round.
-        """
-        random.shuffle(self.nodes)
-        new_labels = {}
-        for node in self.nodes:
-            neighbors = [neb for neb in nx.neighbors(self.graph, node)]
-            pick = self._make_a_pick(neighbors)
-            new_labels[node] = pick
-        self.labels = new_labels
-
     def fit(self, graph):
         """
         Fitting a Label Propagation clustering model.
@@ -53,11 +24,6 @@ class SCD(Estimator):
             * **graph** *(NetworkX graph)* - The graph to be clustered.
         """
         self.graph = graph
-        self.nodes = [node for node in self.graph.nodes()]
-        self.labels = {node: i for i, node in enumerate(self.graph.nodes())}
-        random.seed(self.seed)
-        for _ in range(self.iterations):
-            self._do_a_propagation()
 
     def get_memberships(self):
         r"""Getting the cluster membership of nodes.
