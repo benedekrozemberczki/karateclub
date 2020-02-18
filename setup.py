448d303f83043d0157d80cc50d07a549fa622515
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-02-18 16:26:39 +0000

    MUSAE release

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import find_packages
 setup(
   name = "karateclub",
   packages = find_packages(),
-  version = "0.45.12",
+  version = "0.45.13",
   license = "MIT",
   description = "A general purpose library for community detection, network embedding, and graph mining research.",
   author = "Benedek Rozemberczki",
   author_email = "benedek.rozemberczki@gmail.com",
   url = "https://github.com/benedekrozemberczki/karateclub",
-  download_url = "https://github.com/benedekrozemberczki/karateclub/archive/v_04512.tar.gz",
+  download_url = "https://github.com/benedekrozemberczki/karateclub/archive/v_04513.tar.gz",
   keywords = ["community", "detection", "networkx", "graph", "clustering", "embedding","network","deepwalk","graph2vec","node2vec","deep","learning"],
   install_requires=[
           "numpy",
