25b69f606add4784e073dac10edd37e1db79b4a4
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-02-18 16:15:27 +0000

    MUSAE

--- karateclub/node_embedding/attributed/musae.py
+++ karateclub/node_embedding/attributed/musae.py
@@ -2,15 +2,16 @@ import random
 import numpy as np
 import networkx as nx
 from karateclub.estimator import Estimator
-from gensim.models.word2vec import Word2Vec
+from gensim.models.doc2vec import TaggedDocument, Doc2Vec
 from karateclub.utils.walker import RandomWalker
 
 class MUSAE(Estimator):
-    r"""An implementation of `"MUSAE" <https://arxiv.org/pdf/1810.06768.pdf>`_
-    from the Arxiv '19 paper "SINE: Scalable Incomplete Network Embedding". The 
-    procedure implicitly factorizes a joint adjacency matrix power and feature matrix.
-    The decomposition happens on truncated random walks and the adjacency matrix powers
-    are pooled together.
+    r"""An implementation of `"MUSAE" <https://arxiv.org/abs/1909.13021>`_
+    from the Arxiv '19 paper "MUSAE: Multi-Scale Attributed Node Embedding". The
+    procedure does attributed random walks to approximate the adjacency matrix power
+    node feature matrix products. The matrices are decomposed implicitly by a Skip-Gram
+    style optimizer. The individual embeddings are concatenated together to form a 
+    multi-scale attributed node embedding.
        
     Args:
         walk_number (int): Number of random walks. Default is 5.
@@ -22,8 +23,8 @@ class MUSAE(Estimator):
         learning_rate (float): HogWild! learning rate. Default is 0.05.
         min_count (int): Minimal count of node occurences. Default is 1.
     """
-    def __init__(self, walk_number=10, walk_length=80, dimensions=32, workers=4,
-                 window_size=4, epochs=1, learning_rate=0.05, min_count=1):
+    def __init__(self, walk_number=1, walk_length=10, dimensions=32, workers=4,
+                 window_size=3, epochs=1, learning_rate=0.05, down_sampling=0.0001, min_count=1):
 
         self.walk_number = walk_number
         self.walk_length = walk_length
@@ -32,6 +33,7 @@ class MUSAE(Estimator):
         self.window_size = window_size
         self.epochs = epochs
         self.learning_rate = learning_rate
+        self.down_sampling = down_sampling
         self.min_count = min_count
 
 
@@ -43,7 +45,48 @@ class MUSAE(Estimator):
         return features
 
 
-        
+    def _create_single_embedding(self, document_collections):
+        model = Doc2Vec(document_collections,
+                        vector_size=self.dimensions,
+                        window=0,
+                        min_count=self.min_count,
+                        alpha=self.learning_rate,
+                        dm=0,
+                        sample=self.down_sampling,
+                        workers=self.workers,
+                        epochs=self.epochs)
+
+        emb = np.array([model.docvecs[str(n)] for n in range(self.graph.number_of_nodes())])
+        return emb
+
+
+    def _create_documents(self, features):
+        features_out = [TaggedDocument(words=[str(feat) for feat_elems in feature_set for feat in feat_elems], tags = [str(node)]) for node, feature_set in features.items()]
+        return features_out
+
+    def _setup_musae_features(self, approximation):
+        features = {str(node): [] for node in self.graph.nodes()}
+        for walk in self.walker.walks:
+            for i in range(len(walk)-approximation):
+                source = walk[i]
+                target = walk[i+approximation]
+                features[str(source)].append(self.features[str(target)])
+                features[str(target)].append(self.features[str(source)])
+
+        return self._create_documents(features)
+
+    def _learn_musae_embedding(self):
+
+        for approximation in range(self.window_size):
+
+            features = self._setup_musae_features(approximation+1)
+            embedding = self._create_single_embedding(features)
+            self.embeddings.append(embedding)
+
+    def _create_base_docs(self):
+        features_out = [TaggedDocument(words=[str(feature) for feature in features], tags = [str(node)]) for node, features in self.features.items()]
+        return features_out 
+
     def fit(self, graph, X):
         """
         Fitting a SINE model.
@@ -52,10 +95,13 @@ class MUSAE(Estimator):
             * **graph** *(NetworkX graph)* - The graph to be embedded.
             * **X** *(Scipy COO array)* - The matrix of node features.
         """
+        self.graph = graph
         self.walker = RandomWalker(self.walk_length, self.walk_number)
         self.walker.do_walks(graph)
         self.features = self._feature_transform(graph, X)
-
+        self.base_docs = self._create_base_docs()
+        self.embeddings = [self._create_single_embedding(self.base_docs)]
+        self._learn_musae_embedding()
 
     def get_embedding(self):
         r"""Getting the node embedding.
@@ -63,5 +109,5 @@ class MUSAE(Estimator):
         Return types:
             * **embedding** *(Numpy array)* - The embedding of nodes.
         """
-        embedding = None
+        embedding = np.concatenate(self.embeddings, axis=1)
         return embedding
