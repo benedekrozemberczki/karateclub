a4fde80034698dcb4272ce830a4e9d4563d8e057
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-02-25 19:01:25 +0000

    Karate Club

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import find_packages
 setup(
   name = "karateclub",
   packages = find_packages(),
-  version = "0.45.17",
+  version = "0.45.18",
   license = "MIT",
   description = "A general purpose library for community detection, network embedding, and graph mining research.",
   author = "Benedek Rozemberczki",
   author_email = "benedek.rozemberczki@gmail.com",
   url = "https://github.com/benedekrozemberczki/karateclub",
-  download_url = "https://github.com/benedekrozemberczki/karateclub/archive/v_04517.tar.gz",
+  download_url = "https://github.com/benedekrozemberczki/karateclub/archive/v_04518.tar.gz",
   keywords = ["community", "detection", "networkx", "graph", "clustering", "embedding","network","deepwalk","graph2vec","node2vec","deep","learning"],
   install_requires=[
           "numpy",
