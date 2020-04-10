01bccc48ef985af3937c488511f34257de376887
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-04-10 20:02:12 +0000

    KC

--- karateclub/estimator.py
+++ karateclub/estimator.py
@@ -9,21 +9,26 @@ class Estimator(object):
         """Creatinng an estimator."""
         pass
 
+
     def fit(self):
         """Fitting a model."""
         pass
+
 
     def get_embedding(self):
         """Getting the embeddings (graph or node level)."""
         return None
 
+
     def get_memberships(self):
         """Getting the membership dictionary."""
         return None
 
+
     def get_cluster_centers(self):
         """Getting the cluster centers."""
         return None
+
 
     def _check_networkx_graph(self, graph):
         try:
