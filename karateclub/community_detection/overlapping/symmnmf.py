4cdbe78a57bff930993f41484c8f82ae9451aa77
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-04-20 22:14:56 +0000

    SCD

--- karateclub/community_detection/overlapping/symmnmf.py
+++ karateclub/community_detection/overlapping/symmnmf.py
@@ -59,9 +59,9 @@ class SymmNMF(Estimator):
         """
         number_of_nodes = graph.shape[0]
         non_zero = graph.nonzero()[0].shape[0]
-        self.H = np.random.uniform(0, non_zero/(number_of_nodes**2), size=(number_of_nodes, self.dimensions))
-        self.H_gamma = np.zeros((number_of_nodes, self.dimensions))
-        self.I = np.identity(self.dimensions)
+        self._H = np.random.uniform(0, non_zero/(number_of_nodes**2), size=(number_of_nodes, self.dimensions))
+        self._H_gamma = np.zeros((number_of_nodes, self.dimensions))
+        self._I = np.identity(self.dimensions)
 
     def get_memberships(self):
         r"""Getting the cluster membership of nodes.
@@ -69,7 +69,7 @@ class SymmNMF(Estimator):
         Return types:
             * **memberships** *(dict)* - Node cluster memberships.
         """
-        index = np.argmax(self.W, axis=1)
+        index = np.argmax(self._W, axis=1)
         memberships = {int(i): int(index[i]) for i in range(len(index))}
         return memberships
 
@@ -79,7 +79,7 @@ class SymmNMF(Estimator):
         Return types:
             * **embedding** *(Numpy array)* - The embedding of nodes.
         """
-        embedding = self.H
+        embedding = self._H
         return embedding
 
     def _do_admm_update(self, A_hat):
@@ -87,13 +87,13 @@ class SymmNMF(Estimator):
         Doing a single ADMM update with the adjacency matrix.
         
         """
-        H_covar = np.linalg.inv(self.H.T.dot(self.H) + self.rho*self.I)
-        self.W = (A_hat.dot(A_hat.T.dot(self.H)) + self.rho*self.H - self.H_gamma).dot(H_covar)
-        self.W = np.maximum(self.W, 0)
-        W_covar = np.linalg.inv(self.W.T.dot(self.W) + self.rho*self.I)
-        self.H = (A_hat.dot(A_hat.T.dot(self.W)) + self.rho*self.W + self.H_gamma).dot(W_covar)
-        self.H = np.maximum(self.H, 0)
-        self.H_gamma = self.H_gamma + self.rho*(self.W-self.H)
+        H_covar = np.linalg.inv(self._H.T.dot(self._H) + self.rho*self._I)
+        self._W = (A_hat.dot(A_hat.T.dot(self._H)) + self.rho*self._H - self._H_gamma).dot(H_covar)
+        self._W = np.maximum(self._W, 0)
+        W_covar = np.linalg.inv(self._W.T.dot(self._W) + self.rho*self._I)
+        self._H = (A_hat.dot(A_hat.T.dot(self._W)) + self.rho*self._W + self._H_gamma).dot(W_covar)
+        self._H = np.maximum(self._H, 0)
+        self._H_gamma = self._H_gamma + self.rho*(self._W-self._H)
 
     def fit(self, graph):
         """
