89854eeb83dd7764419b708d2c45a9d1f40df7d0
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-05-02 11:04:14 +0000

    GraphWave different test.

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import find_packages
 setup(
   name = "karateclub",
   packages = find_packages(),
-  version = "1.0.0",
+  version = "1.0.1",
   license = "MIT",
   description = "A general purpose library for community detection, network embedding, and graph mining research.",
   author = "Benedek Rozemberczki",
   author_email = "benedek.rozemberczki@gmail.com",
   url = "https://github.com/benedekrozemberczki/karateclub",
-  download_url = "https://github.com/benedekrozemberczki/karateclub/archive/v_10000.tar.gz",
+  download_url = "https://github.com/benedekrozemberczki/karateclub/archive/v_10001.tar.gz",
   keywords = ["community", "detection", "networkx", "graph", "clustering", "embedding","network","deepwalk","graph2vec","node2vec","deep","learning","louvain","machine-learning","deep-learning","deeplearning"],
   install_requires=[
           "numpy",
