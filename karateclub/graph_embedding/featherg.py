71a73914b71a3b149fe98080c9a5c0e133d73790
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-05-16 13:54:44 +0000

    Feather G

--- karateclub/graph_embedding/featherg.py
+++ karateclub/graph_embedding/featherg.py
@@ -49,8 +49,7 @@ class FeatherG(Estimator):
         """
         A = nx.adjacency_matrix(graph, nodelist=range(graph.number_of_nodes()))
         D_inverse = self._create_D_inverse(graph)
-        A_hat = sparse.identity(graph.number_of_nodes()) + D_inverse.dot(A)
-        A_hat = 0.5*A_hat
+        A_hat = D_inverse.dot(A)
         return A_hat
 
 
@@ -79,85 +78,13 @@ class FeatherG(Estimator):
             * **X** *(NumPy array)* - The node features.
         """
         log_degree = np.array([math.log(graph.degree(node)+1) for node in range(graph.number_of_nodes())]).reshape(-1, 1)
-        eccentricity = np.array([nx.eccentricity(graph,node) for node in range(graph.number_of_nodes())]).reshape(-1, 1)
         clustering_coefficient = np.array([nx.clustering(graph,node) for node in range(graph.number_of_nodes())]).reshape(-1, 1)
         X = np.concatenate([log_degree, eccentricity, clustering_coefficient], axis=1)
         return X
 
-
-    def _get_zero_order_features(self, X):
+    def _calculate_feather(self, graph):
         """
-        Calculating the zero-th order graph features.
-
-        Arg types:
-            * **X** *(NumPy array)* - The node features.
-
-        Return types:
-            * **features** *(NumPy vector)* - The zero-th order graph features.
-        """
-        features = []
-        X = np.abs(X)
-        for col in range(X.shape[1]):
-            x = np.abs(X[:, col])
-            for power in range(1,self.order+1):
-                features.append(np.sum(np.power(x,power)))
-        features = np.array(features).reshape(-1)
-        return features
-
-
-    def _get_first_order_features(self, Psi, X):
-        """
-        Calculating the first order graph features.
-
-        Arg types:
-            * **Psi** *(List of Scipy arrays)* - The wavelet matrices.
-            * **X** *(NumPy array)* - The node features.
-
-        Return types:
-            * **features** *(NumPy vector)* - The first order graph features.
-        """
-        features = []
-        X = np.abs(X)
-        for col in range(X.shape[1]):
-            x = np.abs(X[:, col])
-            for psi in Psi:
-                filtered_x = psi.dot(x)
-                for q in range(1,self.moments):
-                    features.append(np.sum(np.power(np.abs(filtered_x),q)))
-        features = np.array(features).reshape(-1) 
-        return features  
-
-
-    def _get_second_order_features(self, Psi, X):
-        """
-        Calculating the second order graph features.
-
-        Arg types:
-            * **Psi** *(List of Scipy arrays)* - The wavelet matrices.
-            * **X** *(NumPy array)* - The node features.
-
-        Return types:
-            * **features** *(NumPy vector)* - The second order graph features.
-        """
-        features = []
-        X = np.abs(X)
-        for col in range(X.shape[1]):
-            x = np.abs(X[:, col])
-            for i in range(self.order-1):
-                for j in range(i+1, self.order):
-                    psi_j = Psi[i]
-                    psi_j_prime = Psi[j]                 
-                    filtered_x = np.abs(psi_j_prime.dot(np.abs(psi_j.dot(x))))
-                    for q in range(1,self.moments):
-                       features.append(np.sum(np.power(np.abs(filtered_x),q)))
-
-        features = np.array(features).reshape(-1)
-        return features   
-
-
-    def _calculate_geoscattering(self, graph):
-        """
-        Calculating the features of a graph.
+        Calculating the characteristic function features of a graph.
 
         Arg types:
             * **graph** *(NetworkX graph)* - A graph to be embedded.
@@ -166,12 +93,8 @@ class FeatherG(Estimator):
             * **features** *(Numpy vector)* - The embedding of a single graph.
         """
         A_hat = self._get_normalized_adjacency(graph)
-        Psi = self._calculate_wavelets(A_hat)
         X = self._create_node_feature_matrix(graph)
-        zero_order_features = self._get_zero_order_features(X)
-        first_order_features = self._get_first_order_features(Psi, X)
-        second_order_features = self._get_second_order_features(Psi, X)
-        features = np.concatenate([zero_order_features, first_order_features, second_order_features], axis=0)
+
         return features
 
 
@@ -183,7 +106,7 @@ class FeatherG(Estimator):
             * **graphs** *(List of NetworkX graphs)* - The graphs to be embedded.
         """
         self._check_graphs(graphs)
-        self._embedding = [self._calculate_geoscattering(graph) for graph in graphs]
+        self._embedding = [self._calculate_feather(graph) for graph in graphs]
 
 
     def get_embedding(self):
