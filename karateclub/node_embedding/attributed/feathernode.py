273b964393a64c1070ee32de8f3687115f30a074
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-05-16 23:01:41 +0000

    Feather Node

--- karateclub/node_embedding/attributed/feathernode.py
+++ karateclub/node_embedding/attributed/feathernode.py
@@ -15,13 +15,50 @@ class FeatherNode(Estimator):
         reduction_dimensions (int): SVD reduction dimensions. Default is 64.
         svd_iterations (int): SVD iteration count. Default is 20.
         seed (int): Random seed. Default is 42.
+        theta_max (float): Maximal evaluation point. Default is 2.5.
+        eval_points (int): Number of characteristic function evaluation points. Default is 25.
+        order (int): Scale - number of adjacency matrix powers. Default is 5.
     """
     def __init__(self, dimensions=32, reduction_dimensions=64, svd_iterations=20,
-                 seed=42):
+                 seed=42, theta_max=2.5, eval_points=25, order=5):
         self.dimensions = dimensions
         self.reduction_dimensions = reduction_dimensions
         self.svd_iterations = svd_iterations
         self.seed = seed
+        self.theta_max = theta_max
+        self.eval_points = eval_points
+        self.order = order
+
+    def _create_D_inverse(self, graph):
+        """
+        Creating a sparse inverse degree matrix.
+        
+        Arg types:
+            * **graph** *(NetworkX graph)* - The graph to be embedded.
+        Return types:
+            * **D_inverse** *(Scipy array)* - Diagonal inverse degree matrix.
+        """
+        index = np.arange(graph.number_of_nodes())
+        values = np.array([1.0/graph.degree[node] for node in range(graph.number_of_nodes())])
+        shape = (graph.number_of_nodes(), graph.number_of_nodes())
+        D_inverse = sparse.coo_matrix((values, (index, index)), shape=shape)
+        return D_inverse
+
+    def _create_A_tilde(self, graph):
+        """
+        Creating a sparse normalized adjacency matrix.
+        
+        Arg types:
+            * **graph** *(NetworkX graph)* - The graph to be embedded.
+        Return types:
+            * **A_tilde** *(Scipy array)* - The normalized adjacency matrix.
+        """
+        A = nx.adjacency_matrix(graph, nodelist = range(graph.number_of_nodes()))
+        D_inverse = self._create_D_inverse(graph) 
+        A_tilde = D_inverse.dot(A)
+        return A_tilde
+
+
 
     def _create_reduced_features(self, X):
         """
@@ -50,5 +87,15 @@ class FeatherNode(Estimator):
         """
         self._check_graph(graph)
         X = self._create_reduced_features(X)
+        theta = np.linspace(0.01, self.theta_max, self.eval_points)
+        A_tilde = self._create_A_tilde(graph)
+        X = np.outer(X, theta)
+        X = X.reshape(graph.number_of_nodes(), -1)
+        X = np.concatenate([np.cos(X), np.sin(X)], axis=1)
+        feature_blocks = []
+        for _ in range(self.order):
+            X = A_tilde.dot(X)
+            feature_blocks.append(X)
+        self._X = np.concatenate(feature_blocks, axis=1)
 
 
