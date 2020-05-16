bed355b75902aad4afa7e97382f58a10fb19a6f4
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-05-16 14:18:24 +0000

    Feather G

--- examples.py
+++ examples.py
@@ -16,6 +16,20 @@ from karateclub.node_embedding.structural import GraphWave, Role2Vec
 from karateclub.node_embedding.meta import NEU
 from karateclub.dataset import GraphReader, GraphSetReader
 
+#-------------------
+#
+#-------------------
+
+graphs = [nx.newman_watts_strogatz_graph(50, 5, 0.3) for _ in range(100)]
+
+model = FeatherGraph()
+
+model.fit(graphs)
+embedding = model.get_embedding()
+
+print(embedding.shape)
+
+quit()
 
 #----------------
 # GEMSEC example
