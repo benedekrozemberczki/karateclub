87c59983f45bc731f653024122aa7f1de58ad315
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-05-28 10:16:09 +0000

    Try catch correction

--- karateclub/estimator.py
+++ karateclub/estimator.py
@@ -31,43 +31,28 @@ class Estimator(object):
 
 
     def _check_networkx_graph(self, graph):
-        try:
-            if not isinstance(graph, nx.classes.graph.Graph):
-                raise TypeError("This is not a NetworkX graph. Please see requirements.")
-        except:
-                exit("This is not a NetworkX graph. Please see requirements.")
-   
+        if not isinstance(graph, nx.classes.graph.Graph):
+            raise TypeError("This is not a NetworkX graph. Please see requirements.")
 
     def _check_connectivity(self, graph):
         """Checking the connected nature of a single graph."""
-        try:
-            connected = nx.is_connected(graph)
-            if not connected:
-                raise ValueError("Graph is not connected. Please see requirements.")
-        except:
-            exit("Graph is not connected. Please see requirements.")
-
+        connected = nx.is_connected(graph)
+        if not connected:
+            raise ValueError("Graph is not connected. Please see requirements.")
 
     def _check_directedness(self, graph):
         """Checking the undirected nature of a single graph."""
-        try:
-            directed = nx.is_directed(graph)
-            if directed:
-                raise ValueError("Graph is directed. Please see requirements.")
-        except:
-            exit("Graph is directed. Please see requirements.")
+        directed = nx.is_directed(graph)
+        if directed:
+            raise ValueError("Graph is directed. Please see requirements.")
 
 
     def _check_indexing(self, graph):
         """Checking the consecutive numeric indexing."""
         numeric_indices = [index for index in range(graph.number_of_nodes())]
         node_indices = sorted([node for node in graph.nodes()])
-        try:
-           if numeric_indices != node_indices:
-               raise ValueError("The node indexing is wrong. Please see requirements.")
-        except:
-           exit("The node indexing is wrong. Please see requirements.")     
-
+        if numeric_indices != node_indices:
+            raise ValueError("The node indexing is wrong. Please see requirements.")
 
     def _check_graph(self, graph):
         """Check the Karate Club assumptions about the graph."""
@@ -78,9 +63,6 @@ class Estimator(object):
 
     def _check_graphs(self, graphs):
         """Check the Karate Club assumptions for a list of graphs."""
-        try:
-            for graph in graphs:
-                self._check_graph(graph)
-        except:
-            exit("The graph list does not satisfy requirements.")
+        for graph in graphs:
+            self._check_graph(graph)
 
