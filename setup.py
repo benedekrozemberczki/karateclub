6a0f73b41b6367d9aef8a44501684ec0a3bc88ad
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-06-03 23:07:35 +0000

    Seed fix.

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import find_packages
 setup(
   name = "karateclub",
   packages = find_packages(),
-  version = "1.0.5",
+  version = "1.0.6",
   license = "MIT",
   description = "A general purpose library for community detection, network embedding, and graph mining research.",
   author = "Benedek Rozemberczki",
   author_email = "benedek.rozemberczki@gmail.com",
   url = "https://github.com/benedekrozemberczki/karateclub",
-  download_url = "https://github.com/benedekrozemberczki/karateclub/archive/v_10005.tar.gz",
+  download_url = "https://github.com/benedekrozemberczki/karateclub/archive/v_10006.tar.gz",
   keywords = ["community", "detection", "networkx", "graph", "clustering", "embedding","network","deepwalk","graph2vec","node2vec","deep","learning","louvain","machine-learning","deep-learning","deeplearning"],
   install_requires=[
           "numpy",
