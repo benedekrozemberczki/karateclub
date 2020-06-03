a391cdb0fee687361676f17274d59ee30570da68
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-06-03 22:37:16 +0000

    Seed fix.

--- karateclub/node_embedding/neighbourhood/deepwalk.py
+++ karateclub/node_embedding/neighbourhood/deepwalk.py
@@ -20,9 +20,10 @@ class DeepWalk(Estimator):
         epochs (int): Number of epochs. Default is 1.
         learning_rate (float): HogWild! learning rate. Default is 0.05.
         min_count (int): Minimal count of node occurences. Default is 1.
+        seed (int): Random seed value. Default is 42.
     """
     def __init__(self, walk_number=10, walk_length=80, dimensions=128, workers=4,
-                 window_size=5, epochs=1, learning_rate=0.05, min_count=1):
+                 window_size=5, epochs=1, learning_rate=0.05, min_count=1, seed=42):
 
         self.walk_number = walk_number
         self.walk_length = walk_length
@@ -32,6 +33,7 @@ class DeepWalk(Estimator):
         self.epochs = epochs
         self.learning_rate = learning_rate
         self.min_count = min_count
+        self.seed = seed
 
     def fit(self, graph):
         """
@@ -40,6 +42,7 @@ class DeepWalk(Estimator):
         Arg types:
             * **graph** *(NetworkX graph)* - The graph to be embedded.
         """
+        self._set_seed()
         self._check_graph(graph)
         walker = RandomWalker(self.walk_length, self.walk_number)
         walker.do_walks(graph)
@@ -51,7 +54,8 @@ class DeepWalk(Estimator):
                          size=self.dimensions,
                          window=self.window_size,
                          min_count=self.min_count,
-                         workers=self.workers)
+                         workers=self.workers,
+                         seed=self.seed)
 
         num_of_nodes = graph.number_of_nodes()
         self._embedding = [model[str(n)] for n in range(num_of_nodes)]
