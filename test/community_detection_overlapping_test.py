d2bc3fb12ad54e79fb43b29a784c384403a6deef
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-06-10 17:01:40 +0000

    SYMMNMF fix

--- test/community_detection_overlapping_test.py
+++ test/community_detection_overlapping_test.py
@@ -109,3 +109,26 @@ def test_mmf():
 
     assert embedding.shape[0] == graph.number_of_nodes()
     assert embedding.shape[1] == model.dimensions
+
+
+def test_symmnmf():
+    """
+    Test the symmnmf procedure.
+    """
+    graph = nx.newman_watts_strogatz_graph(100, 5, 0.3)
+
+    model = SymmNMF()
+
+    model.fit(graph)
+    memberships = model.get_memberships()
+    
+    indices = [k for k, v in memberships.items()].sort()
+    nodes = [node for node in graph.nodes()].sort()
+
+    assert graph.number_of_nodes() == len(memberships)
+    assert indices == nodes
+
+    embedding = model.get_embedding()
+
+    assert embedding.shape[0] == graph.number_of_nodes()
+    assert embedding.shape[1] == model.dimensions
