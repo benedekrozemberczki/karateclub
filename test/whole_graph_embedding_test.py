7850210f1e530b4fb4f87fd487ee3302aaef3c8c
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-06-10 02:55:37 +0000

    Travis

--- test/whole_graph_embedding_test.py
+++ test/whole_graph_embedding_test.py
@@ -46,7 +46,22 @@ def test_graph2vec():
     assert embedding.shape[0] == len(graphs)
     assert embedding.shape[1] == model.dimensions
 
-def test_fgsd():
+    graphs = []
+
+    for i in range(50):
+        graph = nx.newman_watts_strogatz_graph(50, 5, 0.3)
+        nx.set_node_attributes(graph, {j: str(j) for j in range(50)}, "feature")
+        graphs.append(graph)
+
+    model = Graph2Vec(attributed=True)
+
+    model.fit(graphs)
+    embedding = model.get_embedding()
+    
+    assert embedding.shape[0] == len(graphs)
+    assert embedding.shape[1] == model.dimensions
+
+def test_gl2vec():
     """
     Test the GL2Vec embedding.
     """
@@ -59,3 +74,31 @@ def test_fgsd():
     
     assert embedding.shape[0] == len(graphs)
     assert embedding.shape[1] == model.dimensions
+
+def test_sf():
+    """
+    Test the SF embedding.
+    """
+    graphs = [nx.newman_watts_strogatz_graph(50, 5, 0.3) for _ in range(100)]
+
+    model = SF()
+
+    model.fit(graphs)
+    embedding = model.get_embedding(dimensions=8)
+    
+    assert embedding.shape[0] == len(graphs)
+    assert embedding.shape[1] == model.dimensions
+
+def test_netlsd():
+    """
+    Test the NetLSD embedding.
+    """
+    graphs = [nx.newman_watts_strogatz_graph(50, 5, 0.3) for _ in range(100)]
+
+    model = NetLSD()
+
+    model.fit(graphs)
+    embedding = model.get_embedding()
+    
+    assert embedding.shape[0] == len(graphs)
+    assert embedding.shape[1] == model.steps
