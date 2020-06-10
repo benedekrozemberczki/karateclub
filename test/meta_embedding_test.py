aa3dacbd7348be1264d59dc4e98f21417759f21b
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-06-10 18:02:01 +0000

    NEU test

---
+++ test/meta_embedding_test.py
@@ -0,0 +1,16 @@
+import networkx as nx
+from karateclub import NEU, NetMF
+
+
+def test_neu():
+    """
+    Test the NEU meta embedding class.
+    """
+    graph = nx.newman_watts_strogatz_graph(100, 20, 0.05)
+
+    model = NetMF()
+    meta_model = NEU()
+    meta_model.fit(graph, model)
+    embedding = meta_model.get_embedding()
+
+    assert embedding.shape[0] == graph.number_of_nodes()
