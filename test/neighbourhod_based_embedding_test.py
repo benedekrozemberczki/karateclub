aefd5044e0a52120b87fe300cae9599e444d1f9e
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-06-10 01:10:53 +0000

    Travis

--- test/neighbourhod_based_embedding_test.py
+++ test/neighbourhod_based_embedding_test.py
@@ -31,5 +31,21 @@ def test_walklets():
 
     assert embedding.shape[0] == graph.number_of_nodes()
     assert embedding.shape[1] == model.dimensions*model.window_size
+
+
+def test_hope():
+    """
+    Testing the HOPE class.
+    """
+    model = HOPE()
+
+    graph = nx.watts_strogatz_graph(100, 10, 0.5)
+
+    model.fit(graph)
+
+    embedding = model.get_embedding()
+
+    assert embedding.shape[0] == graph.number_of_nodes()
+    assert embedding.shape[1] == model.dimensions
     
 
