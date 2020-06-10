9af05e56e8a75de038f402ed3f82e2d4b0de0d0e
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-06-10 00:51:02 +0000

    Travis

---
+++ test/generic_test.py
@@ -0,0 +1,19 @@
+import networkx as nx
+
+from karateclub import DeepWalk
+
+def test_sampler():
+    """
+    Testing the sampler base class.
+    """
+    model = DeepWalk()
+
+    graph = nx.watts_strogatz_graph(200, 10, 0)
+
+    model.fit(graph)
+
+    embedding = model.get_embedding()
+
+    assert embedding.shape[1] = model.dimensions
+    
+
