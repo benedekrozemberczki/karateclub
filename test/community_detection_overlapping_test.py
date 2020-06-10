987cbab7ea76480af2fe00a33f75011f684d6926
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-06-10 16:54:51 +0000

    NSSED test

--- test/community_detection_overlapping_test.py
+++ test/community_detection_overlapping_test.py
@@ -18,3 +18,26 @@ def test_egonet_splitter():
 
     assert graph.number_of_nodes() == len(memberships)
     assert indices == nodes
+
+
+def test_nnsed():
+    """
+    Test the NNSED procedure.
+    """
+    graph = nx.newman_watts_strogatz_graph(100, 5, 0.3)
+
+    model = NNSED()
+
+    model.fit(graph)
+    memberships = model.get_memberships()
+    
+    indices = [k for k, v in memberships.items()].sort()
+    nodes = [node for node in graph.nodes()].sort()
+
+    assert graph.number_of_nodes() == len(memberships)
+    assert indices == nodes
+
+    embedding = model.get_embedding()
+
+    assert embedding.shape[0] == graph.number_of_nodes()
+    assert embedding.shape[1] == model.dimensions
