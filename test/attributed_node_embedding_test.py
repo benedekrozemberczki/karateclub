e8e4fd46285cc255c482ed0833fc0aeba15cc80f
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-06-10 18:46:33 +0000

    Attributed test

--- test/attributed_node_embedding_test.py
+++ test/attributed_node_embedding_test.py
@@ -43,3 +43,18 @@ def test_feather_node():
 
     assert embedding.shape[0] == graph.number_of_nodes()
     assert embedding.shape[1] == 2*model.order*model.eval_points*model.reduction_dimensions
+
+
+def test_bane():
+    """
+    Testing the BANE node embedding.
+    """
+    graph = nx.newman_watts_strogatz_graph(250, 10, 0.2)
+
+    features = np.random.uniform(0, 1, (250, 256))
+    model = BANE()
+    model.fit(graph, features)
+    embedding = model.get_embedding()
+
+    assert embedding.shape[0] == graph.number_of_nodes()
+    assert embedding.shape[1] == model.dimensions
