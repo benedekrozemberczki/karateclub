aad56b1e288ff483afbeb382f84c47775bfd233d
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-06-21 23:56:01 +0000

    KC

--- test/community_detection_nonoverlapping_test.py
+++ test/community_detection_nonoverlapping_test.py
@@ -75,6 +75,7 @@ def test_GEMSEC():
 
     assert graph.number_of_nodes() == len(memberships)
     assert indices == nodes
+    assert type(memberships) == dict
 
     embedding = model.get_embedding()
 
