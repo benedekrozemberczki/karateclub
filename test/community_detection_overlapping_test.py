1ec23d2fddc3a01beeb8c6d1dd8f5434dddf1960
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-06-21 23:54:43 +0000

    KC

--- test/community_detection_overlapping_test.py
+++ test/community_detection_overlapping_test.py
@@ -36,6 +36,7 @@ def test_nnsed():
 
     assert graph.number_of_nodes() == len(memberships)
     assert indices == nodes
+    assert type(memberships) == dict
 
     embedding = model.get_embedding()
 
