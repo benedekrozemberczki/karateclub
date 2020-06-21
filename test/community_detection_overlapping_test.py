29b5fe50c9249b07c2a3c77ef1d66738258f55b3
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-06-21 23:50:48 +0000

    KC

--- test/community_detection_overlapping_test.py
+++ test/community_detection_overlapping_test.py
@@ -87,6 +87,7 @@ def test_bigclam():
 
     assert embedding.shape[0] == graph.number_of_nodes()
     assert embedding.shape[1] == model.dimensions
+    assert type(embedding) == np.ndarray
 
 
 def test_mmf():
