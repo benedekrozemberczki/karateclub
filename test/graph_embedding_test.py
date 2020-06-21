22c9868273554a386ab93f14b7ba383ec573ae73
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-06-21 23:34:29 +0000

    KC

--- test/graph_embedding_test.py
+++ test/graph_embedding_test.py
@@ -158,3 +158,4 @@ def test_geoscattering():
     
             assert embedding.shape[0] == len(graphs)
             assert embedding.shape[1] == feature_count
+            assert type(embedding) == np.ndarray
