be8aba3edc4d8ed124376e98eeff91f1d2888074
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-07-13 21:10:58 +0100

    Origin

--- test/meta_embedding_test.py
+++ test/meta_embedding_test.py
@@ -1,6 +1,6 @@
 import numpy as np
 import networkx as nx
-from karateclub import NEU, NetMF
+from karateclub import NEU, NetMF, DeepWalk
 
 
 def test_neu():
@@ -10,6 +10,17 @@ def test_neu():
     graph = nx.newman_watts_strogatz_graph(100, 20, 0.05)
 
     model = NetMF()
+    meta_model = NEU()
+    meta_model.fit(graph, model)
+    embedding = meta_model.get_embedding()
+
+    assert embedding.shape[0] == graph.number_of_nodes()
+    assert type(embedding) == np.ndarray
+
+
+    graph = nx.newman_watts_strogatz_graph(200, 20, 0.05)
+
+    model = DeepWalk()
     meta_model = NEU()
     meta_model.fit(graph, model)
     embedding = meta_model.get_embedding()
