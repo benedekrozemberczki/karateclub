bcd34641cd0f6f6243a8bfae3d6cb4e982a70de7
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-07-13 20:55:54 +0100

    Origin

--- test/attributed_node_embedding_test.py
+++ test/attributed_node_embedding_test.py
@@ -114,6 +114,17 @@ def test_tene():
     assert embedding.shape[1] == 2*model.dimensions
     assert type(embedding) == np.ndarray
 
+    graph = nx.newman_watts_strogatz_graph(150, 10, 0.2)
+
+    features = np.random.uniform(0, 1, (150, 128))
+    model = TENE(dimensions=8)
+    model.fit(graph, features)
+    embedding = model.get_embedding()
+
+    assert embedding.shape[0] == graph.number_of_nodes()
+    assert embedding.shape[1] == 2*model.dimensions
+    assert type(embedding) == np.ndarray
+
 
 def test_tadw():
     """
