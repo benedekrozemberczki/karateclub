cc0281bba566b8a8834b0fa43ecf8f95ae7ecdda
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-07-13 20:44:57 +0100

    Origin

--- test/neighbourhod_based_node_embedding_test.py
+++ test/neighbourhod_based_node_embedding_test.py
@@ -49,6 +49,18 @@ def test_walklets():
     assert embedding.shape[1] == model.dimensions*model.window_size
     assert type(embedding) == np.ndarray
 
+    model = Walklets(dimensions=32)
+
+    graph = nx.watts_strogatz_graph(100, 10, 0.5)
+
+    model.fit(graph)
+
+    embedding = model.get_embedding()
+
+    assert embedding.shape[0] == graph.number_of_nodes()
+    assert embedding.shape[1] == model.dimensions*model.window_size
+    assert type(embedding) == np.ndarray
+
 
 def test_hope():
     """
