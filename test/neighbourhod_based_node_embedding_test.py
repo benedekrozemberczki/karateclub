c962a0b2dcf4390414265dbf4abddea6fc48b337
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-07-13 20:51:17 +0100

    Origin

--- test/neighbourhod_based_node_embedding_test.py
+++ test/neighbourhod_based_node_embedding_test.py
@@ -225,6 +225,18 @@ def test_laplacianeigenmaps():
     assert embedding.shape[1] == model.dimensions
     assert type(embedding) == np.ndarray
 
+    model = LaplacianEigenmaps(dimensions=16)
+
+    graph = nx.watts_strogatz_graph(200, 10, 0.5)
+
+    model.fit(graph)
+
+    embedding = model.get_embedding()
+
+    assert embedding.shape[0] == graph.number_of_nodes()
+    assert embedding.shape[1] == model.dimensions
+    assert type(embedding) == np.ndarray
+
 
 def test_nmf_admm():
     """
