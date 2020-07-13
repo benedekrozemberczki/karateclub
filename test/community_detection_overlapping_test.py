386a560b8ce2b3b467594dd340983ab4002bfd1a
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-07-13 21:04:34 +0100

    Origin

--- test/community_detection_overlapping_test.py
+++ test/community_detection_overlapping_test.py
@@ -251,3 +251,23 @@ def test_symmnmf():
     assert embedding.shape[0] == graph.number_of_nodes()
     assert embedding.shape[1] == model.dimensions
     assert type(embedding) == np.ndarray
+
+    graph = nx.newman_watts_strogatz_graph(200, 5, 0.3)
+
+    model = SymmNMF()
+
+    model.fit(graph)
+    memberships = model.get_memberships()
+    
+    indices = [k for k, v in memberships.items()].sort()
+    nodes = [node for node in graph.nodes()].sort()
+
+    assert graph.number_of_nodes() == len(memberships)
+    assert indices == nodes
+    assert type(memberships) == dict
+
+    embedding = model.get_embedding()
+
+    assert embedding.shape[0] == graph.number_of_nodes()
+    assert embedding.shape[1] == model.dimensions
+    assert type(embedding) == np.ndarray
