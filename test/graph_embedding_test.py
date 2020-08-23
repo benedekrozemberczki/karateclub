ca376d03a288f84a2bf5afe5f87abb87f9618ffb
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-08-23 13:28:02 +0000

    IGE

--- test/graph_embedding_test.py
+++ test/graph_embedding_test.py
@@ -1,6 +1,7 @@
 import numpy as np
 import networkx as nx
-from karateclub.graph_embedding import Graph2Vec, FGSD, GL2Vec, SF, NetLSD, GeoScattering, FeatherGraph
+from karateclub.graph_embedding import Graph2Vec, FGSD, GL2Vec, SF, IGE
+from karateclub.graph_embedding import NetLSD, GeoScattering, FeatherGraph
 
 
 def test_feather_graph():
@@ -216,6 +217,30 @@ def test_geoscattering():
         for moment in range(4, 7):
 
             model = GeoScattering(order=order, moments=moment)
+
+            model.fit(graphs)
+            embedding = model.get_embedding()
+
+            first_block = 3*model.order
+            second_block = 3*(model.order+1)*(model.moments-1)
+            third_block = 3*(model.order-1)*model.order*(model.moments-1)/2
+
+            feature_count = first_block+second_block+third_block
+    
+            assert embedding.shape[0] == len(graphs)
+            assert embedding.shape[1] == feature_count
+            assert type(embedding) == np.ndarray
+
+def test_ige():
+    """
+    Test the IGE embedding.
+    """
+    graphs = [nx.newman_watts_strogatz_graph(50, 5, 0.3) for _ in range(10)]
+
+    for order in range(4, 20):
+        for moment in range(4, 7):
+
+            model = IGE(order=order, moments=moment)
 
             model.fit(graphs)
             embedding = model.get_embedding()
