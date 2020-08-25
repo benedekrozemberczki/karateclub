f785228f24ea27719ed58248262bfb6a1c91dfa9
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-08-25 17:05:18 +0000

    Deezer

--- karateclub/dataset/dataset_reader.py
+++ karateclub/dataset/dataset_reader.py
@@ -17,6 +17,7 @@ class GraphReader(object):
             (:obj:`"facebook"`, :obj:`"twitch"`, :obj:`"wikipedia"`, :obj:`"github"`). Default is 'wikipedia'.
     """
     def __init__(self, dataset: str="wikipedia"):
+        assert dataset in ["wikipedia", "twitch", "github", "facebook","lastfm"] == True
         self.dataset = dataset
         self.base_url = "https://github.com/benedekrozemberczki/karateclub/raw/master/dataset/node_level/"
 
