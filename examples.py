fb2db8eca7f2cfcfee1eddb8e8635940f14f191a
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-08-31 16:29:43 +0000

    [EXAMPLES] add NMFADMM

--- examples.py
+++ examples.py
@@ -7,7 +7,7 @@ import community
 import numpy as np
 from scipy.sparse import coo_matrix
 
-from karateclub.node_embedding.neighbourhood import NMFADMM, NetMF, BoostNE, LaplacianEigenmaps, NodeSketch
+from karateclub.node_embedding.neighbourhood import NetMF, BoostNE, LaplacianEigenmaps, NodeSketch
 from karateclub.community_detection.overlapping import EgoNetSplitter, NNSED, DANMF, MNMF, BigClam, SymmNMF
 from karateclub.community_detection.non_overlapping import EdMot, LabelPropagation, SCD, GEMSEC
 from karateclub.graph_embedding import Graph2Vec, FGSD, GL2Vec, SF, NetLSD, GeoScattering, FeatherGraph, IGE
@@ -303,18 +303,6 @@ model.get_embedding()
 g = nx.newman_watts_strogatz_graph(100, 20, 0.05)
 
 model = BoostNE()
-
-model.fit(g)
-model.get_embedding()
-
-
-#-------------------
-# NMF-ADMM example
-#-------------------
-
-g = nx.newman_watts_strogatz_graph(100, 20, 0.05)
-
-model = NMFADMM()
 
 model.fit(g)
 model.get_embedding()
