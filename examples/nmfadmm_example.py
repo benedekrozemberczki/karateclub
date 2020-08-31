994b6d05b7b85d027446812e498e01ba45188b0c
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-08-31 16:29:25 +0000

    [EXAMPLES] add NMFADMM

---
+++ examples/nmfadmm_example.py
@@ -0,0 +1,10 @@
+"""NetMF illustrative example."""
+
+import networkx as nx
+from karateclub.node_embedding.neighbourhood import NMFADMM
+
+g = nx.newman_watts_strogatz_graph(100, 20, 0.05)
+
+model = NMFADMM()
+
+model.fit(g)
