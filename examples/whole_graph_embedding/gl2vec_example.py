fec0f2bdc62e1c381f7ede6865478a3785faf3bc
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-08-31 19:24:20 +0000

    [EXAMPLES] add new examples

--- examples/whole_graph_embedding/gl2vec_example.py
+++ examples/whole_graph_embedding/gl2vec_example.py
@@ -3,23 +3,7 @@
 import networkx as nx
 from karateclub.graph_embedding import GL2Vec
 
-# GL2Vec attributed example
-
-graphs = []
-
-for i in range(50):
-    graph = nx.newman_watts_strogatz_graph(50, 5, 0.3)
-    nx.set_node_attributes(graph, {j: str(j) for j in range(50)}, "feature")
-    graphs.append(graph)
-model = GL2Vec(attributed=True)
-
-model.fit(graphs)
-model.get_embedding()
-
-
-# GL2Vec generic example
-
-graphs = [nx.newman_watts_strogatz_graph(50, 5, 0.3) for _ in range(1000)]
+graphs = [nx.newman_watts_strogatz_graph(50, 5, 0.3) for _ in range(100)]
 
 model = GL2Vec()
 
