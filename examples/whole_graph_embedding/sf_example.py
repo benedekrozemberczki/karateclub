597ad5e3f102b0d3022a4d77b81a3090123596be
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-09-03 10:24:40 +0000

    [EXAMPLES] Add NetLSD example

--- examples/whole_graph_embedding/sf_example.py
+++ examples/whole_graph_embedding/sf_example.py
@@ -3,7 +3,7 @@
 import networkx as nx
 from karateclub.graph_embedding import SF
 
-graphs = [nx.newman_watts_strogatz_graph(50, 5, 0.3) for _ in range(1000)]
+graphs = [nx.newman_watts_strogatz_graph(50, 5, 0.3) for _ in range(100)]
 
 model = SF()
 
