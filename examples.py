888515f2f18c14c2d8e71d4d66cfd6841dfbce7e
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-09-03 14:16:08 +0000

    [EXAMPLES] Add example

--- examples.py
+++ examples.py
@@ -9,25 +9,6 @@ from scipy.sparse import coo_matrix
 from karateclub.node_embedding.attributed import BANE, TENE, TADW, FSCNMF, SINE, MUSAE
 
 
-#------------------------------------
-# MUSAE example
-#------------------------------------
-
-g = nx.newman_watts_strogatz_graph(50, 10, 0.2)
-
-X = {i: random.sample(range(150),50) for i in range(50)}
-
-row = np.array([k for k, v in X.items() for val in v])
-col = np.array([val for k, v in X.items() for val in v])
-data = np.ones(50*50)
-shape = (50, 150)
-
-X = coo_matrix((data, (row, col)), shape=shape)
-
-model = MUSAE()
-
-model.fit(g, X)
-
 #--------------
 # SINE example
 #--------------
