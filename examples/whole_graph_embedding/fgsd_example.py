c7fae813dfc51aea6103c62157ed38fa2892705b
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-09-03 10:23:17 +0000

    [EXAMPLES] Add NetLSD example

---
+++ examples/whole_graph_embedding/fgsd_example.py
@@ -0,0 +1,11 @@
+"""FGSD illustrative example."""
+
+import networkx as nx
+from karateclub.graph_embedding import FGSD
+
+graphs = [nx.newman_watts_strogatz_graph(50, 5, 0.3) for _ in range(1000)]
+
+model = FGSD()
+
+model.fit(graphs)
+model.get_embedding()
