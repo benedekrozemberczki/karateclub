4dcdde2960ce5e312b0d1f249c456ed9a0dd95ca
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-09-03 14:21:26 +0000

    [EXAMPLES] Add example

---
+++ examples/attributed_node_embedding/fscnmf_example.py
@@ -0,0 +1,16 @@
+"""FCNMF example."""
+
+import random
+import numpy as np
+import networkx as nx
+from scipy.sparse import coo_matrix
+from karateclub.node_embedding.attributed import FSCNMF
+
+
+g = nx.newman_watts_strogatz_graph(200, 20, 0.05)
+
+x = np.random.uniform(0, 1, (200, 200))
+
+model = FSCNMF()
+
+model.fit(g, x)
