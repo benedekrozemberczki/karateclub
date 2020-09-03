974628442ccc9b76728a2bb476a57d30dd9fde39
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-09-03 12:35:08 +0000

    [EXAMPLES] Add example

--- examples/community_detection/scd_example.py
+++ examples/community_detection/scd_example.py
@@ -1,10 +1,10 @@
-"""DANMF example."""
+"""SCD example."""
 
 import networkx as nx
-from karateclub.community_detection.overlapping import DANMF
+from karateclub.community_detection.non_overlapping import SCD
 
 g = nx.newman_watts_strogatz_graph(100, 20, 0.05)
 
-model = DANMF()
+model = SCD()
 
 model.fit(g)
