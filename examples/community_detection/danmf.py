8cae8aaa8e962c4007ef98cfe885ad004f89fb63
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-09-03 12:21:31 +0000

    [EXAMPLES] Add GW example

--- examples/community_detection/danmf.py
+++ examples/community_detection/danmf.py
@@ -1,10 +1,10 @@
-"""Ego Splitting example."""
+"""DANMF example."""
 
 import networkx as nx
-from karateclub.community_detection.overlapping import EgoNetSplitter
+from karateclub.community_detection.overlapping import DANMF
 
 g = nx.newman_watts_strogatz_graph(100, 20, 0.05)
 
-model = EgoNetSplitter()
+model = DANMF()
 
 model.fit(g)
