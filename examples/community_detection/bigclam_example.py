1cd1be560e21983d4f9343b4a344b302a2a62d6c
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-09-03 12:30:20 +0000

    [EXAMPLES] Add example

--- examples/community_detection/bigclam_example.py
+++ examples/community_detection/bigclam_example.py
@@ -1,10 +1,10 @@
-"""MNMF example."""
+"""BigClam example."""
 
 import networkx as nx
-from karateclub.community_detection.overlapping import MNMF
+from karateclub.community_detection.overlapping import BigClam
 
 g = nx.newman_watts_strogatz_graph(100, 20, 0.05)
 
-model = MNMF()
+model = BigClam()
 
 model.fit(g)
