065f45d9450ff5948bbd13e1311f938fa11f0ae5
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-09-03 12:36:33 +0000

    [EXAMPLES] Add example

--- examples/community_detection/edmot_example.py
+++ examples/community_detection/edmot_example.py
@@ -1,7 +1,7 @@
 """EdMot example."""
 
 import networkx as nx
-from karateclub.community_detection.overlapping import EdMot
+from karateclub.community_detection.non_overlapping import EdMot
 
 g = nx.newman_watts_strogatz_graph(100, 20, 0.05)
 
