e999fdb1032957559d0da22067feed2ed8abae0f
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-09-03 12:29:56 +0000

    [EXAMPLES] Add example

--- examples/community_detection/symmnmf_example.py
+++ examples/community_detection/symmnmf_example.py
@@ -1,10 +1,10 @@
-"""MNMF example."""
+"""SymmNMF example."""
 
 import networkx as nx
-from karateclub.community_detection.overlapping import MNMF
+from karateclub.community_detection.overlapping import SymmNMF
 
 g = nx.newman_watts_strogatz_graph(100, 20, 0.05)
 
-model = MNMF()
+model = SymmNMF()
 
 model.fit(g)
