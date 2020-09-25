ae9731d1019ae79d10234dee49fce2f96cd8723b
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-09-25 09:57:41 +0000

    ASNE

--- karateclub/node_embedding/attributed/asne.py
+++ karateclub/node_embedding/attributed/asne.py
@@ -1,116 +1,90 @@
+import random
 import numpy as np
 import networkx as nx
 from typing import Union
-from numpy.linalg import inv
 from scipy.sparse import coo_matrix
-from sklearn.decomposition import TruncatedSVD
 from karateclub.estimator import Estimator
+from gensim.models.word2vec import Word2Vec
+from karateclub.utils.walker import RandomWalker
 
-class BANE(Estimator):
-    r"""An implementation of `"BANE" <https://shiruipan.github.io/publication/yang-binarized-2018/yang-binarized-2018.pdf>`_
-    from the ICDM '18 paper "Binarized Attributed Network Embedding Class". The 
-    procedure first calculates the truncated SVD of an adjacency - feature matrix
-    product. This matrix is further decomposed by a binary CCD based technique. 
+class SINE(Estimator):
+    r"""An implementation of `"SINE" <https://arxiv.org/pdf/1810.06768.pdf>`_
+    from the ICDM '18 paper "SINE: Scalable Incomplete Network Embedding". The 
+    procedure implicitly factorizes a joint adjacency matrix power and feature matrix.
+    The decomposition happens on truncated random walks and the adjacency matrix powers
+    are pooled together.
        
     Args:
-        dimensions (int): Number of embedding dimensions. Default is 32.
-        svd_iterations (int): SVD iteration count. Default is 20.
-        seed (int): Random seed. Default is 42.
-        alpha (float): Kernel matrix inversion parameter. Default is 0.3. 
-        iterations (int): Matrix decomposition iterations. Default is 100.
-        binarization_iterations (int): Binarization iterations. Default is 20.
+        walk_number (int): Number of random walks. Default is 10.
+        walk_length (int): Length of random walks. Default is 80.
+        dimensions (int): Dimensionality of embedding. Default is 128.
+        workers (int): Number of cores. Default is 4.
+        window_size (int): Matrix power order. Default is 5.
+        epochs (int): Number of epochs. Default is 1.
+        learning_rate (float): HogWild! learning rate. Default is 0.05.
+        min_count (int): Minimal count of node occurrences. Default is 1.
         seed (int): Random seed value. Default is 42.
     """
-    def __init__(self, dimensions: int=32, svd_iterations: int=20, seed: int=42,
-                 alpha: float=0.3, iterations: int=100, binarization_iterations: int=20):
+    def __init__(self, walk_number: int=10, walk_length: int=80, dimensions: int=128,
+                 workers: int=4, window_size: int=5, epochs: int=1,
+                 learning_rate: float=0.05, min_count: int=1, seed: int=42):
 
+        self.walk_number = walk_number
+        self.walk_length = walk_length
         self.dimensions = dimensions
-        self.svd_iterations = svd_iterations
+        self.workers = workers
+        self.window_size = window_size
+        self.epochs = epochs
+        self.learning_rate = learning_rate
+        self.min_count = min_count
         self.seed = seed
-        self.alpha = alpha
-        self.iterations = iterations
-        self.binarization_iterations = binarization_iterations
-        self.seed = seed
 
-    def _create_target_matrix(self, graph):
-        """
-        Creating a normalized sparse adjacency matrix target.
 
-        Arg types:
-            * **graph** *(NetworkX graph)* - The graph to be embedded. 
+    def _feature_transform(self, graph, X):
+        features = {str(node): [] for node in graph.nodes()}
+        nodes = X.row
+        for i, node in enumerate(nodes):
+            features[str(node)].append("feature_"+ str(X.col[i]))
+        return features
 
-        Return types:
-            * **P** *(Scipy COO matrix) - The target matrix.    
-        """
-        weighted_graph = nx.Graph()
-        for (u, v) in graph.edges():
-            weighted_graph.add_edge(u, v, weight=1.0/graph.degree(u))
-            weighted_graph.add_edge(v, u, weight=1.0/graph.degree(v))
-        P = nx.adjacency_matrix(weighted_graph,
-                                nodelist=range(graph.number_of_nodes()))
-        return P
-
+    def _select_walklets(self):
+        self._walklets = []
+        for walk in self._walker.walks:
+            for power in range(1, self.window_size+1): 
+                for step in range(power+1):
+                    neighbors = [n for i, n in enumerate(walk[step:]) if i % power == 0]
+                    neighbors = [n for n in neighbors for _ in range(0, 3)]
+                    neighbors = [random.choice(self._features[val]) if i % 3 == 1 and self._features[val] else val for i, val in enumerate(neighbors)]
+                    self._walklets.append(neighbors)
+        del self._walker
+        
+        
     def fit(self, graph: nx.classes.graph.Graph, X: Union[np.array, coo_matrix]):
         """
-        Fitting a BANE model.
+        Fitting a SINE model.
 
         Arg types:
             * **graph** *(NetworkX graph)* - The graph to be embedded.
-            * **X** *(Scipy COO or Numpy array)* - The matrix of node features.
+            * **X** *(Scipy COO array)* - The matrix of node features.
         """
         self._set_seed()
         self._check_graph(graph)
-        self._P = self._create_target_matrix(graph)
-        self._X = X
-        self._fit_base_SVD_model()
-        self._binary_optimize()
+        self._walker = RandomWalker(self.walk_length, self.walk_number)
+        self._walker.do_walks(graph)
+        self._features = self._feature_transform(graph, X)
+        self._select_walklets()
 
-    def _fit_base_SVD_model(self):
-        """
-        Reducing the dimensionality with SVD in the 1st step.
-        """
-        self._P = self._P.dot(self._X)
-        self.model = TruncatedSVD(n_components=self.dimensions,
-                                  n_iter=self.svd_iterations,
-                                  random_state=self.seed)
+        model = Word2Vec(self._walklets,
+                         hs=0,
+                         alpha=self.learning_rate,
+                         iter=self.epochs,
+                         size=self.dimensions,
+                         window=1,
+                         min_count=self.min_count,
+                         workers=self.workers,
+                         seed=self.seed)
 
-        self.model.fit(self._P)
-        self._P = self.model.fit_transform(self._P)
-
-    def _update_G(self):
-        """
-        Updating the kernel matrix.
-        """
-        self._G = np.dot(self._B.transpose(), self._B)
-        self._G = self._G + self.alpha*np.eye(self.dimensions)
-        self._G = inv(self._G)
-        self._G = self._G.dot(self._B.transpose()).dot(self._P)
-
-    def _update_Q(self):
-        """
-        Updating the rescaled target matrix.
-        """
-        self._Q = self._G.dot(self._P.transpose()).transpose()
-
-    def _update_B(self):
-        """
-        Updating the embedding matrix.
-        """
-        for _ in range(self.iterations):
-            for d in range(self.dimensions):
-                sel = [x for x in range(self.dimensions) if x != d]
-                self._B[:, d] = self._Q[:, d]-self._B[:, sel].dot(self._G[sel, :]).dot(self._G[:, d]).transpose()
-                self._B[:, d] = np.sign(self._B[:, d])
-
-    def _binary_optimize(self):
-        """
-        Starting 2nd optimization phase with power iterations and CCD.
-        """
-        self._B = np.sign(np.random.normal(size=(self._P.shape[0], self.dimensions)))
-        for _ in range(self.binarization_iterations):
-            self._update_G()
-            self._update_Q()
-            self._update_B()
+        self.embedding = np.array([model[str(n)] for n in range(graph.number_of_nodes())])
 
     def get_embedding(self) -> np.array:
         r"""Getting the node embedding.
@@ -118,5 +92,5 @@ class BANE(Estimator):
         Return types:
             * **embedding** *(Numpy array)* - The embedding of nodes.
         """
-        embedding = self._B
+        embedding = self.embedding
         return embedding
