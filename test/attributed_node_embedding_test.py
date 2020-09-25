7331c2edbb219379a384f02182aaa934bd8112a5
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-09-25 10:00:15 +0000

    ASNE

--- test/attributed_node_embedding_test.py
+++ test/attributed_node_embedding_test.py
@@ -2,7 +2,7 @@ import random
 import numpy as np
 import networkx as nx
 from scipy.sparse import coo_matrix
-from karateclub.node_embedding.attributed import BANE, TENE, TADW, FSCNMF, SINE, MUSAE, FeatherNode
+from karateclub.node_embedding.attributed import BANE, TENE, TADW, FSCNMF, SINE, MUSAE, FeatherNode, ASNE
 
 
 def test_feather_node():
@@ -176,6 +176,29 @@ def test_musae():
 
 
 def test_sine():
+    """
+    Testing the SINE node embedding.
+    """
+    graph = nx.newman_watts_strogatz_graph(100, 10, 0.2)
+
+    features = {i: random.sample(range(150), 50) for i in range(100)}
+    row = np.array([k for k, v in features.items() for val in v])
+    col = np.array([val for k, v in features.items() for val in v])
+    data = np.ones(100*50)
+    shape = (100, 150)
+
+    features = coo_matrix((data, (row, col)), shape=shape)
+
+    model = SINE()
+    model.fit(graph, features)
+    embedding = model.get_embedding()
+
+    assert embedding.shape[0] == graph.number_of_nodes()
+    assert embedding.shape[1] == model.dimensions
+    assert type(embedding) == np.ndarray
+
+
+def test_asne():
     """
     Testing the SINE node embedding.
     """
