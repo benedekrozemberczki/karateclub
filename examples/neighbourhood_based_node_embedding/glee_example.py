83b1b769315d9b7666241bdf3a0fd9df6637373e
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-10-18 16:40:53 +0100

    [GLEE]

--- examples/neighbourhood_based_node_embedding/glee_example.py
+++ examples/neighbourhood_based_node_embedding/glee_example.py
@@ -1,10 +1,10 @@
-"""Laplacian Eigenmaps illustrative example."""
+"""GLEE illustrative example."""
 
 import networkx as nx
-from karateclub.node_embedding.neighbourhood import LaplacianEigenmaps
+from karateclub.node_embedding.neighbourhood import GLEE
 
 g = nx.newman_watts_strogatz_graph(1000, 20, 0.05)
 
-model = LaplacianEigenmaps()
+model = GLEE()
 
 model.fit(g)
