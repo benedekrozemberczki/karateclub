59273226338c3f2b2b56df44d4c93d8f5eda54bb
BenTenmann <benji.tenmann@me.com>
2021-08-05 20:51:49 +0100

    feat: added weighted / directed to node2vec
    added functions to generalise node2vec for weighted and/or directed graphs

--- karateclub/utils/walker.py
+++ karateclub/utils/walker.py
@@ -1,6 +1,10 @@
 import random
+from functools import partial
+from typing import List, Callable
+
 import numpy as np
 import networkx as nx
+
 
 class RandomWalker:
     """
@@ -10,6 +14,7 @@ class RandomWalker:
         walk_length (int): Number of random walks.
         walk_number (int): Number of nodes in truncated walk.
     """
+
     def __init__(self, walk_length: int, walk_number: int):
         self.walk_length = walk_length
         self.walk_number = walk_number
@@ -25,7 +30,7 @@ class RandomWalker:
             * **walk** *(list of strings)* - A single truncated random walk.
         """
         walk = [node]
-        for _ in range(self.walk_length-1):
+        for _ in range(self.walk_length - 1):
             nebs = [node for node in self.graph.neighbors(walk[-1])]
             if len(nebs) > 0:
                 walk = walk + random.sample(nebs, 1)
@@ -46,6 +51,53 @@ class RandomWalker:
                 walk_from_node = self.do_walk(node)
                 self.walks.append(walk_from_node)
 
+
+def _check_value(value, name):
+    try:
+        _ = 1 / value
+
+    except ZeroDivisionError:
+        raise ValueError(f"The value of {name} is too small "
+                         f"or zero to be used in 1/{name}.")
+
+
+def _undirected(node, graph) -> List[tuple]:
+    edges = graph.edges(node)
+
+    return edges
+
+
+def _directed(node, graph) -> List[tuple]:
+    edges = graph.out_edges(node, data=True)
+
+    return edges
+
+
+def _get_edge_fn(graph) -> Callable:
+    fn = _directed if nx.classes.function.is_directed(graph) \
+        else _undirected
+
+    fn = partial(fn, graph=graph)
+    return fn
+
+
+def _unweighted(edges: List[tuple]) -> np.ndarray:
+    return np.ones(len(edges))
+
+
+def _weighted(edges: List[tuple]) -> np.ndarray:
+    weights = map(lambda edge: edge[-1]['weight'], edges)
+
+    return np.array([*weights])
+
+
+def _get_weight_fn(graph) -> Callable:
+    fn = _weighted if nx.classes.function.is_weighted(graph) \
+        else _unweighted
+
+    return fn
+
+
 class BiasedRandomWalker:
     """
     Class to do biased second order random walks.
@@ -53,24 +105,25 @@ class BiasedRandomWalker:
     Args:
         walk_length (int): Number of random walks.
         walk_number (int): Number of nodes in truncated walk.
-        p (float): Return parameter (1/p transition probability) to move towards from previous node.
+        p (float): Return parameter (1/p transition probability) to move towards previous node.
         q (float): In-out parameter (1/q transition probability) to move away from previous node.
     """
+    walks: list
+    graph: nx.classes.graph.Graph
+    edge_fn: Callable
+    weight_fn: Callable
+
     def __init__(self, walk_length: int, walk_number: int, p: float, q: float):
         self.walk_length = walk_length
         self.walk_number = walk_number
-        try: 
-            _ = 1/p
-        except ZeroDivisionError:
-            raise ValueError("The value of p is too small or zero to be used in 1/p.")
+
+        _check_value(p, 'p')
         self.p = p
-        try: 
-            _ = 1/q
-        except ZeroDivisionError:
-            raise ValueError("The value of q is too small or zero to be used in 1/q.")
+
+        _check_value(q, 'q')
         self.q = q
 
-    def do_walk(self, node):
+    def do_walk(self, node: int) -> List[str]:
         """
         Doing a single truncated second order random walk from a source node.
 
@@ -83,21 +136,31 @@ class BiasedRandomWalker:
         walk = [node]
         previous_node = None
         previous_node_neighbors = []
-        for _ in range(self.walk_length-1):
+        for _ in range(self.walk_length - 1):
             current_node = walk[-1]
-            current_node_neighbors = np.array(list(self.graph.neighbors(current_node)))
-            probability = np.array([1/self.q] * len(current_node_neighbors), dtype=float)
-            probability[current_node_neighbors==previous_node] = 1/self.p
-            probability[(np.isin(current_node_neighbors, previous_node_neighbors))] = 1
-            norm_probability = probability/sum(probability)
+            edges = self.edge_fn(current_node)
+            current_node_neighbors = np.array([edge[1] for edge in edges])
+
+            weights = self.weight_fn(edges)
+            probability = np.piecewise(weights,
+                                       [current_node_neighbors == previous_node,
+                                        np.isin(current_node_neighbors, previous_node_neighbors)],
+                                       [lambda w: w / self.p,
+                                        lambda w: w / 1,
+                                        lambda w: w / self.q]
+                                       )
+
+            norm_probability = probability / sum(probability)
             selected = np.random.choice(current_node_neighbors, 1, p=norm_probability)[0]
             walk.append(selected)
+
             previous_node_neighbors = current_node_neighbors
             previous_node = current_node
+
         walk = [str(w) for w in walk]
         return walk
 
-    def do_walks(self, graph):
+    def do_walks(self, graph) -> None:
         """
         Doing a fixed number of truncated random walk from every node in the graph.
 
@@ -106,7 +169,12 @@ class BiasedRandomWalker:
         """
         self.walks = []
         self.graph = graph
+
+        self.edge_fn = _get_edge_fn(graph)
+        self.weight_fn = _get_weight_fn(graph)
+
         for node in self.graph.nodes():
             for _ in range(self.walk_number):
                 walk_from_node = self.do_walk(node)
                 self.walks.append(walk_from_node)
+
