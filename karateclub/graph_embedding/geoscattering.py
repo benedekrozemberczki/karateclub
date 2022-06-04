76d5397896bc6bdd159da8f691bab92a139a70bf
Rozemberczki <kmdb028@astrazeneca.net>
2022-06-04 17:47:45 +0100

    GeoScattering infer

--- karateclub/graph_embedding/geoscattering.py
+++ karateclub/graph_embedding/geoscattering.py
@@ -205,3 +205,17 @@ class GeoScattering(Estimator):
             * **embedding** *(Numpy array)* - The embedding of graphs.
         """
         return np.array(self._embedding)
+
+    def infer(self, graphs: List[nx.classes.graph.Graph]) -> np.array:
+        r"""Infer the embedding of graphs.
+
+        Arg types:
+            * **graphs** *(List of NetworkX graphs)* - The graphs to be embedded.
+
+        Return types:
+            * **embedding** *(Numpy array)* - The embedding of graphs.
+        """
+        self._set_seed()
+        graphs = self._check_graphs(graphs)
+        embedding = np.array([self._calculate_geoscattering(graph) for graph in graphs])
+        return embedding
