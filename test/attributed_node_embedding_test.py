edaadd1fbffa37002096dfdf676e66341c931cb8
tomlincr <christopher.tomlinson@ucl.ac.uk>
2023-08-10 18:11:48 +0100

    fix sine test stochastic therefore check shape

--- test/attributed_node_embedding_test.py
+++ test/attributed_node_embedding_test.py
@@ -241,7 +241,7 @@ def test_sine():
     assert embedding.shape[0] == graph.number_of_nodes()
     assert embedding.shape[1] == model.dimensions
     assert type(embedding) == np.ndarray
-    assert np.array_equal(model.fit_transform(graph, features), embedding)
+    assert np.array_equal(model.fit_transform(graph, features).shape, embedding.shape)
 
 
 def test_ae():
